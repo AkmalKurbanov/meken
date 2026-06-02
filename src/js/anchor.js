4dc1274be72306f421e41b1d43df31c8e60e61cf
akmalworkfront <akmal.work.front@gmail.com>
2026-06-02 13:26:28 +0600

    fix about section

--- src/js/anchor.js
+++ src/js/anchor.js
@@ -1,29 +1,27 @@
 const anchorLinks = document.querySelectorAll('a[href^="#"]');
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 
 if (anchorLinks.length > 0) {
-  anchorLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
+  anchorLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
       e.preventDefault();
 
-      const targetId = this.getAttribute('href');
-      
-      if (targetId !== '#' && targetId.startsWith('#')) {
+      const targetId = this.getAttribute("href");
+
+      if (targetId !== "#" && targetId.startsWith("#")) {
         const targetElement = document.querySelector(targetId);
 
         if (targetElement) {
-          
           const headerHeight = header ? header.offsetHeight : 0;
-          
-          
-          const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
-          
-          
+
+          const elementPosition =
+            targetElement.getBoundingClientRect().top + window.scrollY;
+
           const offsetPosition = elementPosition - headerHeight;
 
           window.scrollTo({
             top: offsetPosition,
-            behavior: 'smooth'
+            behavior: "smooth",
           });
         }
       }
